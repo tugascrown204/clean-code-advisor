f326b741a7f8e51325043329784e6f1b8938777f
tugascrown204 <gajdertevet@hotmail.com>
2025-09-24 17:33:35 +0330

    feat: initial commit for src/analyzer.js

---
+++ src/analyzer.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const analyzeCode = (directory) => {
+    // Analyze code logic here
+    console.log(`Analyzing code in: ${directory}`);
+};
+
+module.exports = { analyzeCode };
